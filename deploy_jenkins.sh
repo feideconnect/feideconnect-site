e6b6a44de7bfe7e6e463578224e61d8a74ba558f
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-07-27 12:09:59 +0200

    Added blue-green deploy of connect-site for feideconnect.no
    Actually uses connect-site and connect-site-old rather than blue/green but should work just the same

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e # halt script on error
+app=$(jq -r .name nova.config.json)
+oldapp="${app}-old"
+domain="feideconnect.no"
 
-
-echo "Ready to deploy updated version of feideconnect.no"
+echo "Ready to deploy updated version of ${domain}"
 
 
 . ~/cf-login.sh
@@ -13,9 +15,26 @@ gem install rouge
 
 npm install
 node_modules/bower/bin/bower install --config.interactive=false -p
+if cf app "${app}" |egrep -q '#.*running'
+then
+    first='n'
+    if cf app "${oldapp}"
+    then
+        cf delete -f "${oldapp}"
+    fi
+    cf rename "${app}" "${oldapp}"
+else
+    first='y'
+fi
 node_modules/grunt-cli/bin/grunt publish
 
-cf map-route connect-site feideconnect.no
+cf map-route "${app}" "${domain}"
+
+if [ "${first}" = 'n' ]
+then
+    cf unmap-route "${oldapp}" "${domain}"
+    cf stop "${oldapp}"
+fi
 
 echo "Done."
 
